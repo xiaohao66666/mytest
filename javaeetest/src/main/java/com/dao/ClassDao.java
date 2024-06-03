32566a3a598258ab7ed4a373d489235ebdf8a18f
xiaohao66666 <3013080501@qq.com>
2024-06-03 20:25:22 +0800

    Delete javaeetest directory

--- javaeetest/src/main/java/com/dao/ClassDao.java
+++
@@ -1,9 +0,0 @@
-package com.dao;
-
-import com.bean.Clazz;
-
-import java.util.List;
-
-public interface ClassDao {
-    public List<Clazz>  findAll();
-}
--- javaeetest/src/main/java/com/dao/UserDao.java
+++
@@ -1,9 +0,0 @@
-package com.dao;
-
-import com.bean.User;
-import org.apache.ibatis.annotations.Param;
-
-public interface UserDao {
-    public User login(@Param("username") String username, @Param("password") String password);
-    public int register(@Param("username") String username, @Param("password") String password);
-}
